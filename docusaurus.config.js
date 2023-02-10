36ae55a2e7e7f7d04b453c5c273dba6ab63c55af
JasonMa <jasonma0012@foxmail.com>
2023-02-10 19:34:13 +0800

    no message

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Mooa Toon',
-  tagline: 'Amazing Toon Rendering in UE5',
+  tagline: 'The Ultimate Solution for UE5 Cinematic Toon Rendering',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -102,7 +102,7 @@ const config = {
             items: [
               {
                 label: 'Discord',
-                href: 'https://discord.gg/DDU8CQZjCh',
+                href: 'https://discord.gg/rHmhgdtGfn',
               },
               {
                 label: 'Twitter',
